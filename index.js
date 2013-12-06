12947c05ceeac6945fdc78f954ea952a2e1230a3
Eric Schoffstall <contra@wearefractal.com>
2013-12-06 15:48:35 -0700

    follow gulp plugin guidelines
    errors need to be reported and file.contents should always be a buffer.

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ module.exports = function() {
     var svgo = new (require('svgo'))({ plugins: [arguments[0]] });
     return es.map(function(file, cb) {
         svgo.optimize(String(file.contents), function(result) {
-            // If SVGO throws an error, pipe the original stream instead
-            file.contents = (result.error) ? String(file.contents) : result.data;
+            if (result.error) return cb(result.error);
+            file.contents = new Buffer(result.data);
             cb(null, file);
         });
     });
