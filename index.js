b6c98d5ed1d5d60c9e1ee92efb896bb760c2d16a
Ben Briggs <therealbenbriggs@hotmail.com>
2013-12-23 21:25:19 +0000

    Fixed formatting + jshint.

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
+/* jshint node:true */
+
+'use strict';
+
 var es = require('event-stream');
 
 module.exports = function() {
-    'use strict';
     var svgo = new (require('svgo'))({ plugins: [arguments[0]] });
     return es.map(function(file, cb) {
         svgo.optimize(String(file.contents), function(result) {
-            if (result.error) return cb(result.error);
+            if (result.error) {
+                return cb(result.error);
+            }
             file.contents = new Buffer(result.data);
             cb(null, file);
         });
