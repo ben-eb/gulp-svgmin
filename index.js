7fa6e924a909d8278aa351965862681e41215e0b
Nicolas Froidure <nicolas_froidure@elitwork.com>
2014-01-04 21:53:45 +0100

    Added streams support

--- index.js
+++ index.js
@@ -2,17 +2,67 @@
 
 'use strict';
 
-var map = require('map-stream');
+var Transform = require('stream').Transform
+  , BufferStreams = require('bufferstreams')
+  , SVGOptim = require('svgo')
+  , gutil = require('gulp-util');
 
-module.exports = function() {
-    var svgo = new (require('svgo'))({ plugins: [arguments[0]] });
-    return map(function(file, cb) {
-        svgo.optimize(String(file.contents), function(result) {
+
+// File level transform function
+function minifySVGTransform(svgo) {
+
+    if(!(svgo instanceof SVGOptim)) {
+        svgo = new SVGOptim(svgo);
+    }
+
+    // Return a callback function handling the buffered content
+    return function(err, buf, cb) {
+
+        // Handle any error
+        if(err) cb(gutil.PluginError('svgmin', err));
+
+        // Use the buffered content
+        svgo.optimize(String(buf), function(result) {
             if (result.error) {
-                return cb(result.error);
+                cb(gutil.PluginError('svgmin', result.error));
             }
-            file.contents = new Buffer(result.data);
-            cb(null, file);
+
+            // Bring it back to streams
+            cb(null, new Buffer(result.data));
         });
-    });
+    };
+}
+
+// Plugin function
+function minifySVGGulp(plugins) {
+    var stream = new Transform({objectMode: true});
+    
+    var svgo = new SVGOptim({ plugins: [plugins] });
+
+    stream._transform = function(file, unused, done) {
+        if(file.isStream()) {
+            file.contents = file.contents.pipe(
+                new BufferStreams(minifySVGTransform(svgo)));
+            stream.push(file);
+            done();
+        } else {
+            svgo.optimize(String(file.contents), function(result) {
+                if (result.error) {
+                    stream.emit('error', gutil.PluginError('svgmin', result.error));
+                }
+                file.contents = new Buffer(result.data);
+                stream.push(file);
+                done();
+            });
+        }
+    };
+
+    return stream;
 };
+
+// Export the file level transform function for other plugins usage
+minifySVGGulp.fileTransform = minifySVGTransform;
+
+// Export the plugin main function
+module.exports = minifySVGGulp;
+
