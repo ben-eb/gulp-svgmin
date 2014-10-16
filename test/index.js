038b3a3808ab5c243dc0eadea26b7c1c20583f75
Antoine Bolvy <antoine.bolvy@gmail.com>
2014-10-16 15:19:23 +0200

    Refactor tests

--- test/index.js
+++ test/index.js
@@ -1,157 +1,97 @@
-/* jshint node: true */
-/* global describe, it */
-
 'use strict';
 
-var expect = require('chai').expect,
-    gutil  = require('gulp-util'),
-    svgmin = require('./index'),
-    es = require('event-stream'),
-    Stream = require('stream');
+require('should');
+var gutil  = require('gulp-util');
+var svgmin = require('../lib');
+var es = require('event-stream');
+var Stream = require('stream');
 
+var head = '<?xml version="1.0" encoding="utf-8"?>';
 var doctype = '<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">';
-
-var raw = '<?xml version="1.0" encoding="utf-8"?>' + doctype +
-'<svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xml:space="preserve">' +
-'<circle cx="50" cy="50" r="40" fill="yellow" /><!-- test comment -->' +
-'</svg>';
+var fullsvg = '<svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xml:space="preserve">' +
+              '<circle cx="50" cy="50" r="40" fill="yellow" />' +
+              '<!-- test comment --></svg>';
+var raw = head + doctype + fullsvg;
 
 var compressed = '<svg xmlns="http://www.w3.org/2000/svg"><circle cx="50" cy="50" r="40" fill="#ff00"/></svg>';
 
+var makeTest = function(options, content, expected) {
+  it('in stream mode', function(done) {
+    var stream = svgmin(options);
+    var fakeFile = new gutil.File({
+      contents: new Stream()
+    });
+
+    stream.on('data', function(data) {
+      data.contents.pipe(es.wait(function(err, data) {
+        data.toString().should.match(expected);
+        done();
+      }));
+    });
+
+    stream.write(fakeFile);
+    fakeFile.contents.write(content);
+    fakeFile.contents.end();
+  });
+  it('in buffer mode', function(done) {
+    var stream = svgmin(options);
+    stream.on('data', function(data) {
+      data.contents.toString().should.match(expected);
+      done();
+    });
+
+    stream.write(new gutil.File({
+      contents: new Buffer(content)
+    }));
+  });
+};
+
 describe('gulp-svgmin', function() {
+  describe('with null contents', function() {
+    it('should let null files pass through', function(done) {
+      var stream = svgmin();
+      var i = 0;
 
-    describe('with null contents', function() {
-
-        it('should let null files pass through', function(done) {
-
-            var s = svgmin(),
-                n = 0;
-            s.pipe(es.through(function(file) {
-                  expect(file.path).to.equal('bibabelula.md');
-                  expect(file.contents).to.equal(null);
-                  n++;
-              }, function() {
-                  expect(n).to.equal(1);
-                  done();
-              }));
-            s.write(new gutil.File({
-                path: 'bibabelula.md',
-                contents: null
-            }));
-            s.end();
-
-        });
+      stream.pipe(es.through(function(file) {
+          file.path.should.eql('bibabelula.md');
+          (file.contents === null).should.be.true;
+          i += 1;
+        }, function() {
+          i.should.eql(1);
+          done();
+        }));
+      stream.write(new gutil.File({
+        path: 'bibabelula.md',
+        contents: null
+      }));
+      stream.end();
     });
+  });
 
-    describe('in buffer mode', function() {
+  describe('should minify svg with svgo', function() {
+    makeTest({}, raw, compressed);
+  });
 
-        it('should minify svg with svgo', function(cb) {
-            var stream = svgmin();
-
-            stream.on('data', function(data) {
-                expect(String(data.contents)).to.equal(compressed);
-                cb();
-            });
-
-            stream.write(new gutil.File({
-                contents: new Buffer(raw)
-            }));
-        });
-
-        it('should honor disabling plugins, such as keeping the doctype', function(cb) {
-            var stream = svgmin([{
-                removeDoctype: false
-            }]);
-
-            stream.on('data', function(data) {
-                expect(String(data.contents)).to.have.string(doctype);
-                cb();
-            });
-
-            stream.write(new gutil.File({
-                contents: new Buffer(raw)
-            }));
-        });
-
-        it('should allow disabling multiple plugins', function(cb) {
-            var stream = svgmin([{
-                removeDoctype: false
-            }, {
-                removeComments: false
-            }]);
-
-            stream.on('data', function(data) {
-                expect(String(data.contents)).to.have.string(doctype).and.to.have.string('test comment');
-                cb();
-            });
-
-            stream.write(new gutil.File({
-                contents: new Buffer(raw)
-            }));
-        });
-
+  describe('should honor disabling plugins, such as keeping the doctype', function() {
+    var options = {
+      plugins: [
+        { removeDoctype: false }
+      ]
+    };
+    makeTest(options, raw, function(content) {
+      return content.should.containEql(doctype);
     });
+  });
 
-    describe('stream mode', function() {
-        it('should minify svg with svgo', function(cb) {
-            var stream = svgmin();
-            var fakeFile = new gutil.File({
-                contents: new Stream()
-            });
-        
-            stream.on('data', function(data) {
-                data.contents.pipe(es.wait(function(err, data) {
-                    expect(data).to.equal(compressed);
-                    cb();
-                }));
-            });
-        
-            stream.write(fakeFile);
-            fakeFile.contents.write(raw);
-            fakeFile.contents.end();
-        });
-        
-        it('should honor disabling plugins, such as keeping the doctype', function(cb) {
-            var stream = svgmin([{
-                removeDoctype: false
-            }]);
-            var fakeFile = new gutil.File({
-                contents: new Stream()
-            });
-        
-            stream.on('data', function(data) {
-                data.contents.pipe(es.wait(function(err, data) {
-                    expect(data).to.have.string(doctype);
-                    cb();
-                }));
-            });
-        
-            stream.write(fakeFile);
-            fakeFile.contents.write(raw);
-            fakeFile.contents.end();
-        });
-
-        it('should allow disabling multiple plugins', function(cb) {
-            var stream = svgmin([{
-                removeDoctype: false
-            }, {
-                removeComments: false
-            }]);
-
-            var fakeFile = new gutil.File({
-                contents: new Stream()
-            });
-
-            stream.on('data', function(data) {
-                data.contents.pipe(es.wait(function(err, data) {
-                    expect(data).to.have.string(doctype).and.to.have.string('test comment');
-                    cb();
-                }));
-            });
-
-            stream.write(fakeFile);
-            fakeFile.contents.write(raw);
-            fakeFile.contents.end();
-        });
+  describe('should allow disabling multiple plugins', function() {
+    var options = {
+      plugins: [
+        { removeDoctype: false },
+        { removeComments: false }
+      ]
+    };
+    makeTest(options, raw, function(content) {
+      return content.should.containEql(doctype).and.containEql('test comment');
     });
+  });
 });
