ac195c3571b800b6067b00e64a53ce004a05d815
Antoine Bolvy <antoine.bolvy@gmail.com>
2014-10-16 15:27:45 +0200

    Don't run mocha here

--- gulpfile.js
+++ gulpfile.js
@@ -2,25 +2,22 @@
 
 'use strict';
 
-var gulp    = require('gulp'),
-    gutil   = require('gulp-util'),
-    clear   = require('clear'),
-    mocha   = require('gulp-mocha'),
-    jshint  = require('gulp-jshint'),
-    stylish = require('jshint-stylish');
+var gulp    = require('gulp');
+var gutil   = require('gulp-util');
+var clear   = require('clear');
+var jshint  = require('gulp-jshint');
 
 gulp.task('lint', function () {
-    gulp.src('*.js')
-        .pipe(jshint())
-        .pipe(jshint.reporter('jshint-stylish'))
-        .pipe(mocha());
+  gulp.src('*.js')
+    .pipe(jshint())
+    .pipe(jshint.reporter('jshint-stylish'));
 });
 
 gulp.task('default', function() {
+  gulp.run('lint');
+  gulp.watch('*.js', function(event) {
+    clear();
+    gutil.log(gutil.colors.cyan(event.path.replace(process.cwd(), '')) + ' ' + event.type + '. (' + gutil.colors.magenta(gutil.date('HH:MM:ss')) + ')');
     gulp.run('lint');
-    gulp.watch('*.js', function(event) {
-        clear();
-        gutil.log(gutil.colors.cyan(event.path.replace(process.cwd(), '')) + ' ' + event.type + '. (' + gutil.colors.magenta(gutil.date('HH:MM:ss')) + ')');
-        gulp.run('lint');
-    });
+  });
 });
