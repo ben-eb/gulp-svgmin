9c0cedf3dfca25cc3d761fd942a59f34cb4cb867
Antoine Bolvy <antoine.bolvy@gmail.com>
2014-10-16 15:55:20 +0200

    Update dependencies, fix deprecated uses

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,20 @@
-/* jshint node:true */
-
 'use strict';
 
 var gulp    = require('gulp');
-var gutil   = require('gulp-util');
-var clear   = require('clear');
 var jshint  = require('gulp-jshint');
 
+var paths = {
+  js: ['gulpfile.js', 'lib/**/*.js', 'test/**/*.js']
+};
+
 gulp.task('lint', function () {
-  gulp.src('*.js')
+  gulp.src(paths.js)
     .pipe(jshint())
     .pipe(jshint.reporter('jshint-stylish'));
 });
 
-gulp.task('default', function() {
-  gulp.run('lint');
-  gulp.watch('*.js', function(event) {
-    clear();
-    gutil.log(gutil.colors.cyan(event.path.replace(process.cwd(), '')) + ' ' + event.type + '. (' + gutil.colors.magenta(gutil.date('HH:MM:ss')) + ')');
-    gulp.run('lint');
-  });
+gulp.task('watch', function() {
+  gulp.watch(paths.js, ['lint']);
 });
+
+gulp.task('default', ['lint', 'watch'], function() {});
